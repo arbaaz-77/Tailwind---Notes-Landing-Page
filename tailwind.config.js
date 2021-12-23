bc38071f5bc8d54d031c57bc61dc7c1aeba0d2f5
Arbaaz Sumar <43441056+arbaaz-77@users.noreply.github.com>
2021-12-23 17:54:18 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./src/index.html"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      Poppins: ['Poppins, sans-serif'],
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        lg: "1124px",
+        xl: "1124px",
+        "2xl": "1124px",
+      }
+    }
+  },
+  plugins: [],
+}
